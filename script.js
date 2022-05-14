5d9d0f96575d528690bca28f83084df076013d5f
ldsboram <ldsboram@naver.com>
2022-05-14 20:41:37 +0900

    complete system but need to be added more.

--- script.js
+++ script.js
@@ -45,6 +45,9 @@ function whereToGo(playerNo, now, advance){//playerNo: 플레이어의 번호(0,
     if(now>100 && now%10>3){//주차장 안쪽으로 더 깊이 들어갈 수 없습니다.
         return -1;
     }
+    else if(isPeg(now, players)===playerNo){//해당 위치에 내 말이 있어도 갈 수 없습니다.
+        return -1;
+    }
     else{
         return now;//이 함수는 말판의 현재 위치에서 advance만큼 떨어진 위치의 칸 숫자를 반환합니다.
     }// 그러나 갈 수 있는 칸이 없다면 -1을 반환합니다.
@@ -152,6 +155,174 @@ function wait(sec){
     }
 }
 
+function changePlayer(tothisman){
+    document.querySelectorAll(".player").forEach(player=>{
+        player.innerText='';
+    })
+    document.getElementById("player"+tothisman).innerText="⬇️";
+}
+
+function indicateDice(){
+    dicenum=dice;
+    document.getElementById("dice").src="./diceroll.gif";
+    setTimeout(function(){
+        document.getElementById("dice").src="./diceroll"+dicenum+".png";
+    },1200)
+}
+
+function updateInterface(){
+    while(previousPositions.length) {
+        const position = previousPositions.pop();
+
+        document.getElementById(`pixel-${position}`).innerText = "";
+    }
+
+    const emojis = ["🔵","🔴","🟡","🟢"]
+    players.forEach((player, i) => {
+        const pegs = player.peg;
+
+        pegs.forEach((peg) => {
+            const target = document.getElementById(`pixel-${peg.position}`);
+
+            if(target) {
+                previousPositions.push(peg.position);
+
+                target.innerText = emojis[i];
+            }
+        })
+    })
+
+    if(nowPlayer===0){
+        enableRoll=true;
+    }
+    changePlayer(nowPlayer);
+}
+
+window.addEventListener("load", () => {
+
+    // pixel click
+    document.querySelectorAll(".pixel").forEach(pixel => {
+        /*
+        Fill here
+        */
+        pixel.addEventListener("click", () => {//칸 클릭 시 
+            if(pixel.classList.contains('select')){// 이동 가능한 곳이면(강조된 곳이면)
+                let movingPeg;
+                let advance=dice;
+                if(isAllInHome(nowPlayer,players)){//내 말이 다 집에 있으면 전진량은 6인 셈 침.(집에서 나가기 위해)
+                    advance=6;
+                }
+
+                let movablePeg=0;
+                for(let i=0; i<4; i++){;//어떤 말의 이동인지 계산
+                    if("pixel-"+selectableDest[i]===pixel.id){
+                        movingPeg=i;
+                        movablePeg++;
+                    }
+                }
+
+                if(movablePeg!==1){//다수의 보기가 있는 경우
+                    for(let i=0; i<4; i++){;//어떤 말의 이동인지 계산
+                        if("pixel-"+selectableDest[i]===pixel.id){//다수의 보기 강조하기
+                            document.getElementById("pixel-"+players[nowPlayer].peg[i].position).classList.add('moveable');
+                        }
+                    }
+                    pixel.classList.add('here');
+                    document.querySelectorAll(".pixel").forEach(pixel => {//강조된 칸 되돌리기
+                        pixel.classList.remove('select');
+                    })
+                    return;
+                }
+
+                //말을 움직임.
+                let dest=selectableDest[movingPeg];//그 말이 갈 곳을 계산 후
+                console.log("Player no."+nowPlayer+" moved his peg no."+movingPeg+" which is in square no. "+players[nowPlayer].peg[movingPeg].position+",");
+                let catchedPlayer=moveToThere(nowPlayer,movingPeg,dest,players);//그 칸으로 움직임
+                console.log(" to square no. "+dest+"!");
+        
+                if(catchedPlayer!=-1){
+                    console.log("Player no."+catchedPlayer+" loses a Peg!");
+                }
+
+                document.querySelectorAll(".pixel").forEach(pixel => {//강조된 칸 되돌리기
+                    pixel.classList.remove('select');
+                })
+                updateInterface();
+
+                if(isAllInGoal(nowPlayer,players)){//승리 체크
+                    console.log("Player no."+nowPlayer+" WIN!!");
+                    winFlag=true;
+                    declareOfWin();
+                    return;
+                }
+
+                if(dice!==6){//턴 넘기기
+                    nowPlayer=(nowPlayer+1)%4;
+                }
+                
+                otherTurns();
+            }
+
+            else if(pixel.classList.contains('moveable')){
+                let clickedPeg=0;
+                for(let i=0; i<4; i++){
+                    if(pixel.id==="pixel-"+players[nowPlayer].peg[i].position){
+                        clickedPeg=i;
+                    }
+                }
+
+                let movingPeg=clickedPeg;
+                let dest=selectableDest[movingPeg];//그 말이 갈 곳을 계산 후
+                console.log("Player no."+nowPlayer+" moved his peg no."+movingPeg+" which is in square no. "+players[nowPlayer].peg[movingPeg].position+",");
+                let catchedPlayer=moveToThere(nowPlayer,movingPeg,dest,players);//그 칸으로 움직임
+                console.log(" to square no. "+dest+"!");
+        
+                if(catchedPlayer!=-1){
+                    console.log("Player no."+catchedPlayer+" loses a Peg!");
+                }
+
+                document.querySelectorAll(".pixel").forEach(pixel => {//강조된 칸 되돌리기
+                    pixel.classList.remove('moveable');
+                    pixel.classList.remove('here');
+                })
+                updateInterface();
+
+                if(isAllInGoal(nowPlayer,players)){//승리 체크
+                    console.log("Player no."+nowPlayer+" WIN!!");
+                    winFlag=true;
+                    declareOfWin();
+                    return;
+                }
+
+                if(dice!=6){//주사위 숫자가 6이 아니라면
+                    console.log("Player no."+nowPlayer+" finishes the turn!");
+                    nowPlayer=(nowPlayer+1)%4;//턴 넘김
+                }
+                else{//주사위 숫자가 6이라면 한번 더 할 수 있음.
+                    console.log("Player no."+nowPlayer+" can roll the dice again!");
+                }
+                
+                
+                otherTurns();
+            }
+        })
+    })
+
+    // roll click
+    document.querySelector("#roll").addEventListener("click", () => {
+        if(enableRoll && nowPlayer===0){//내 차례가 아니면 굴려지지 않음
+            Turn();
+        }
+    })
+/*
+    document.querySelector("#skip").addEventListener("click", () => {
+        nowPlayer=(nowPlayer+1)%4;
+        otherTurns();
+    })//디버깅용 버튼
+*/
+
+});
+
 let players = [];
 for(let i=0; i<4; i++){
     players[i]= new Player("AI"+i,i);
@@ -161,70 +332,155 @@ for(let i=0; i<4; i++){
 let turns=0;
 //게임 진행 코드
 let nowPlayer=0;//0번 플레이어부터 시작
-while(true){
+let dice=1;
+let enableRoll=true;
+let winFlag=false;
+let selectableDest=[];
+
+let previousPositions = [];
+updateInterface();
+
+function Turn() {
     console.log("\nPlayer no."+nowPlayer+" takes the turn!");
+    changePlayer(nowPlayer);//상태창 변경
+
+    enableRoll=false;//주사위 굴리기 일시중지
     turns++;
 
-    let dice=1;
+    dice=1;
     if(!isAllInHome(nowPlayer,players)){//말이 전부 집에 있지 않으면
         dice=throwDice();//주사위를 던짐
         console.log("Player no."+nowPlayer+" rolled the dice and the result is "+dice+"!");
+        indicateDice();//주사위 애니메이션
     }
-    let movingPeg=whatToMove(nowPlayer,dice,players);//움직일 말을 고르기
 
-    if(movingPeg!=-1){//움직일 말이 있다면
-        let advance=dice;
-        if(isAllInHome(nowPlayer,players)){//내 말이 다 집에 있으면 전진량은 6인 셈 침.(집에서 나가기 위해)
-            advance=6;
+    if(nowPlayer!==0){//내 차례가 아닌 경우
+        let movingPeg=whatToMove(nowPlayer,dice,players);//움직일 말을 고르기
+
+        if(movingPeg!=-1){//움직일 말이 있다면
+            let advance=dice;
+            if(isAllInHome(nowPlayer,players)){//내 말이 다 집에 있으면 전진량은 6인 셈 침.(집에서 나가기 위해)
+                advance=6;
+            }
+            let dest=whereToGo(nowPlayer,players[nowPlayer].peg[movingPeg].position,advance);//그 말이 갈 곳을 계산 후
+            console.log("Player no."+nowPlayer+" moved his peg no."+movingPeg+" which is in square no. "+players[nowPlayer].peg[movingPeg].position+",");
+            let catchedPlayer=moveToThere(nowPlayer,movingPeg,dest,players);//그 칸으로 움직임
+            console.log(" to square no. "+dest+"!");
+    
+            if(catchedPlayer!=-1){
+                console.log("Player no."+catchedPlayer+" loses a Peg!");
+            }
         }
-        let dest=whereToGo(nowPlayer,players[nowPlayer].peg[movingPeg].position,advance);//그 말이 갈 곳을 계산 후
-        console.log("Player no."+nowPlayer+" moved his peg no."+movingPeg+" which is in square no. "+players[nowPlayer].peg[movingPeg].position+",");
-        let catchedPlayer=moveToThere(nowPlayer,movingPeg,dest,players);//그 칸으로 움직임
-        console.log(" to square no. "+dest+"!");
+        else{//없으면 패스
+            console.log("Nothing to move! PASS!");
+        }
+    
+        console.log("\nState of board:")//디버그용 보드 체커
+        for(let i=0; i<4; i++){
+            let str="Player no."+i+" || ";
+            for(let j=0; j<4; j++){
+                str+="Peg "+j+": "+players[i].peg[j].position+" | ";
+            }
+            console.log(str);
+        }
+    
+        if(isAllInGoal(nowPlayer,players)){//승리 체크
+            console.log("Player no."+nowPlayer+" WIN!!");
+            winFlag=true;
+            declareOfWin();
+            return;
+        }
+    
+        if(dice!=6){//주사위 숫자가 6이 아니라면
+            console.log("Player no."+nowPlayer+" finishes the turn!");
+            nowPlayer=(nowPlayer+1)%4;//턴 넘김
+        }
+        else{//주사위 숫자가 6이라면 한번 더 할 수 있음.
+            console.log("Player no."+nowPlayer+" can roll the dice again!");
+        }
+    
+        
+        setTimeout(updateInterface,1500);//인터페이스 업데이트 후 주사위 굴리기 허용
+    }
+    else{//내 차례인 경우
+        let movingPeg=whatToMove(nowPlayer,dice,players);//움직일 말을 고르기
 
-        if(catchedPlayer!=-1){
-            console.log("Player no."+catchedPlayer+" loses a Peg!");
+        if(movingPeg!=-1){//움직일 말이 있다면
+
+            let advance=dice;
+            if(isAllInHome(nowPlayer,players)){//내 말이 다 집에 있으면 전진량은 6인 셈 침.(집에서 나가기 위해)
+                advance=6;
+            }
+            for(let i=0; i<4; i++){
+                selectableDest[i]=whereToGo(nowPlayer,players[nowPlayer].peg[i].position,advance);//말이 갈 곳을 각각 계산 한 뒤
+            }
+
+            setTimeout(()=>{
+                for(let i=0; i<4; i++){
+                    if(selectableDest[i]!==-1 && isPeg(selectableDest[i],players).ownerNo!==0){
+                        document.getElementById("pixel-"+selectableDest[i]).classList.add('select');
+                    }
+                }
+            },1250)//주사위가 굴러간 뒤 표시
+
+        }
+        else{//없으면 패스
+            console.log("Nothing to move! PASS!");
+
+            console.log("\nState of board:")//디버그용 보드 체커
+            for(let i=0; i<4; i++){
+                let str="Player no."+i+" || ";
+                for(let j=0; j<4; j++){
+                    str+="Peg "+j+": "+players[i].peg[j].position+" | ";
+                }
+                console.log(str);
+            }
+        
+            if(isAllInGoal(nowPlayer,players)){//승리 체크
+                console.log("Player no."+nowPlayer+" WIN!!");
+                winFlag=true;
+                declareOfWin();
+                return;
+            }
+        
+            if(dice!=6){//주사위 숫자가 6이 아니라면
+                console.log("Player no."+nowPlayer+" finishes the turn!");
+                nowPlayer=(nowPlayer+1)%4;//턴 넘김
+            }
+            else{//주사위 숫자가 6이라면 한번 더 할 수 있음.
+                console.log("Player no."+nowPlayer+" can roll the dice again!");
+            }
+        
+            
+            setTimeout(updateInterface,1500);//인터페이스 업데이트 후 주사위 굴리기 허용
+            setTimeout(otherTurns,1500);//그 외 턴 진행
         }
     }
-    else{//없으면 패스
-        console.log("Nothing to move! PASS!");
-    }
 
-    console.log("\nState of board:")//디버그용 보드 체커
-    for(let i=0; i<4; i++){
-        let str="Player no."+i+" || ";
-        for(let j=0; j<4; j++){
-            str+="Peg "+j+": "+players[i].peg[j].position+" | ";
-        }
-        console.log(str);
-    }
 
-    if(isAllInGoal(nowPlayer,players)){//승리 체크
-        console.log("Player no."+nowPlayer+" WIN!!");
-        break;
-    }
-
-    if(dice!=6){//주사위 숫자가 6이 아니라면
-        console.log("Player no."+nowPlayer+" finishes the turn!");
-        nowPlayer=(nowPlayer+1)%4;//턴 넘김
-    }
-    else{//주사위 숫자가 6이라면 한번 더 할 수 있음.
-        console.log("Player no."+nowPlayer+" can roll the dice again!");
-    }
-
-    //wait(8);//디버깅용, 8초 대기
     
 }
-console.log(turns+" turns passed to finish the game.");
 
-export{
-    Peg,
-    Player,
-    whereToGo,
-    throwDice,
-    isPeg,
-    isAllInHome,
-    isAllInGoal,
-    whatToMove,
-    moveToThere,
+function otherTurns(){//내 턴이 돌아올 때까지 턴을 계속함
+    if(!(nowPlayer===0 || winFlag)){
+        Turn();
+    }
+    var timer=setInterval(function(){
+        if(nowPlayer===0 || winFlag){
+            clearInterval(timer);
+        }
+        if(!(nowPlayer===0 || winFlag)){
+            Turn();
+        }
+        if(nowPlayer===0 || winFlag){
+            clearInterval(timer);
+        }
+    },2000) 
 }
+
+function declareOfWin(){// 승리 시 선언되는 함수
+    updateInterface();
+    alert(nowPlayer+" Win!!")
+}
+
+
